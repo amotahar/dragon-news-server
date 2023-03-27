34ce08ec10ad213e30f7e07694c72db47e55967e
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-03-27 16:31:20 +0600

    rujtyuyt

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 
 const port = process.env.PORT || 5000;
+
+app.use(cors());
 
 const categories = require('./data/categories.json')
 
