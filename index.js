b3439ab3829d0ea56fc960c8452cd566c0507eaf
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-03-27 19:49:56 +0600

    dsfghsdfhygshg

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json')
+//const categories = require('./data/categories.json')
+
 
 
 app.get('/', (req, res) => {
