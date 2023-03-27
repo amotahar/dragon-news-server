d1bd570c936f9fd6e00deb8d5e7f0cdb7cce23be
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-03-27 13:16:08 +0600

    ddsfsfsd

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+const categories = require('./data/categories.json')
+
+
+app.get('/', (req, res) => {
+     res.send('News API is running')
+})
+
+
+app.get('/news-categories', (req, res) => {
+    res.send(categories)
+})
+
+
+app.listen(port, () => {
+    console.log('Dragon News Server is running on Port', port);
+})
