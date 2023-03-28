ea42b09813806634696da2668808563a26bd2da5
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-03-28 13:38:12 +0600

    fg

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require('./data/categories.json')
-//const categories = require('./data/categories.json')
+const categories = require("./data/categories.json");
+const news = require("./data/news.json");
 
+app.get("/", (req, res) => {
+  res.send("News API is running");
+});
 
+app.get("/news-categories", (req, res) => {
+  res.send(categories);
+});
 
-app.get('/', (req, res) => {
-     res.send('News API is running')
-})
+app.get('./category/:id', (req, res) => {
+  const id = req.params.id;
+  if (id == '08') {
+    res.send(news);
+  } else {
+    const category_news = news.filter(n => n.category_id === id);
+    res.send(category_news);
+  }
+});
 
-
-app.get('/news-categories', (req, res) => {
-    res.send(categories)
-})
-
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
 
 app.listen(port, () => {
-    console.log('Dragon News Server is running on Port', port);
-})
+  console.log("Dragon News Server is running on Port", port);
+});
