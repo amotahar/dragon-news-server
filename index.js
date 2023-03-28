4d020474a02f77edd3effa874bc95a80b8b4d630
Motahar Hossain <amotaharhossain.cse@gmail.com>
2023-03-28 13:58:02 +0600

    gasdfga

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
-
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require("./data/categories.json");
-const news = require("./data/news.json");
+const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
-app.get("/", (req, res) => {
-  res.send("News API is running");
+app.get('/', (req, res) => {
+    res.send('News API Running');
 });
 
-app.get("/news-categories", (req, res) => {
-  res.send(categories);
+app.get('/news-categories', (req, res) => {
+    res.send(categories)
 });
 
-app.get('./category/:id', (req, res) => {
-  const id = req.params.id;
-  if (id == '08') {
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const category_news = news.filter(n => n.category_id === id);
+        res.send(category_news);
+    }
+})
+
+app.get('/news', (req, res) =>{
     res.send(news);
-  } else {
-    const category_news = news.filter(n => n.category_id === id);
-    res.send(category_news);
-  }
 });
 
-app.get("/news/:id", (req, res) => {
-  const id = req.params.id;
-  const selectedNews = news.find((n) => n._id === id);
-  res.send(selectedNews);
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
 });
 
 app.listen(port, () => {
-  console.log("Dragon News Server is running on Port", port);
-});
+    console.log('Dragon News Server running on port', port);
+})
